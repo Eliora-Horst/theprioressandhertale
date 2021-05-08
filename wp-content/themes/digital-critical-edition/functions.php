fe61ba95e9b7573340c539664b2d9ca8b16ea949
Eliora Horst <eliora.horst@gmail.com>
2021-05-08 04:00:51 +0100

    Updated pages to have subtitle

--- wp-content/themes/digital-critical-edition/functions.php
+++ wp-content/themes/digital-critical-edition/functions.php
@@ -34,3 +34,16 @@
 		return $exclude_filters;
 	}
 
+	function DH_custom_logo_setup() {
+    $defaults = array(
+        'height'               => 200,
+        'width'                => 200,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'unlink-homepage-logo' => true, 
+    );
+ 
+    add_theme_support( 'custom-logo', $defaults );
+}
+
